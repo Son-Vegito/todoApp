a717fdf4d6a451b568b0d0b99504d9a7a91df294
son-vegito <devtastudies@gmail.com>
2024-03-23 22:14:26 +0530

    mark as done btn functional

--- frontend/src/components/ToDos.jsx
+++ frontend/src/components/ToDos.jsx
@@ -6,7 +6,18 @@ export function ToDos({todos}){
             return <div>
                 <h2>{todo.title}</h2>
                 <h4>{todo.description}</h4>
-                <button>{(todo.completed==true)?"Completed":"Mark as Completed!"}</button>
+                <button onClick={()=>{
+
+                    fetch("http://localhost:3000/completed",{
+                        method:"PUT",
+                        body:JSON.stringify({
+                            id:todo._id
+                        }),
+                        headers:{
+                            "Content-Type":"application/json",
+                        }
+                    })
+                }}>{(todo.completed==true)?"Completed":"Mark as Completed!"}</button>
             </div>
         })}
     </div>
