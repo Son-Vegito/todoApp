ca321f79283f359f46f22a000f9dba5bd2985c90
son-vegito <devtastudies@gmail.com>
2024-03-24 21:03:18 +0530

    fixed infinite requests

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,15 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import { CreateToDo } from './components/CreateToDo'
 import { ToDos } from './components/ToDos'
 function App() {
   const [todos,setTodos]=useState([]);
 
-  fetch("http://localhost:3000/todo")
+  useEffect(()=>{setInterval(()=>{  fetch("http://localhost:3000/todo")
   .then(async function(res){
     const json=await res.json();
     setTodos(json);
-  })
+  })},5000)},[])
 
   return (
     <div>
